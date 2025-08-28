c62c1305c32658cd20b7d2108e84b835784d132d
rifan <tararifan93@gmail.com>
2025-08-28 14:14:07 +0700

    feat(timer): implement start, stop, & reset
    - Added TimerDisplay component to show elapsed seconds
    - Added TimerControl component with Start/Stop and Reset buttons
    - Implemented useEffect hook to handle timer interval

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 // import { useState } from 'react'
 
-function App() {
+import { useEffect, useState } from "react"
 
+const TimerDisplay = (props) => {
+  return <h1>{props.second}</h1>
+}
+
+const TimerControl = (props) => {
   return (
-    <>
-    
-    </>
+  <>
+    <button onClick={props.onToggle} >{props.isRunning ? 'Stop' : 'Start'}</button>
+    <button onClick={props.onReset} >Reset</button>
+  </>
   )
 }
 
-export default App
+export default function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = () => {
+    setIsRunning(prevIsRunning => !prevIsRunning)
+  };
+
+  const resetTimer = () => {
+    setSeconds(0); setIsRunning(false);
+  };
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds + 1);
+      },1000);
+    }
+    return () => clearInterval(intervalId)
+  }, [isRunning])
+
+  return (
+    <>
+      <div>
+        <TimerDisplay second={seconds}/>
+        <TimerControl onToggle={toggleTimer} onReset={resetTimer} isRunning={isRunning} />
+      </div>
+    </>
+  )
+};
